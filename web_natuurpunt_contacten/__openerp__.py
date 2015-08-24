9f6f4a62a761b8e055f86def218aa15d08dd47c5
Fabian Semal <fabian.semal@smartsolution.be>
2015-08-24 15:01:21 +0200

    [IMP] removes advanced filter section for natuurpunt contacten [IMP] format IBAN account numbers       for list view override _format       for form view format_value

---
+++ web_natuurpunt_contacten/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'web extend for natuurpunt contacten',
+    'category': 'Hidden',    
+    'author' : 'Natuurpunt (joeri.belis@natuurpunt.be)',
+    'description':"""
+OpenERP Web module
+===========================
+removes sidebar section for natuurpunt contacten
+custom IBAN account number format 
+
+""",
+    'version': '1.0',
+    'depends': [],
+    'js': ['static/*/*.js', 'static/*/js/*.js'],
+    'css': [],
+    'auto_install': False,
+    'web_preload': False,
+}
