77216de2cd821077bdeb9d8cf65a735ef5e283c5
Joeri Belis <joeri.Belis@natuurpunt.be>
2020-01-09 10:38:29 +0100

    [IMP] #4179 - module om pages(tabs) rechten in beeld brengen

---
+++ natuurpunt_ir_ui_view_page/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################## 
+
+{
+    "name" : "natuurpunt_ir_ui_view_page.py",
+    "version" : "1.0",
+    "author" : "Natuurpunt (joeri.belis@natuurpunt.be)",
+    "website" : "www.natuurpunt.be",
+    "category" : "Base extention",
+    "description": """
+    test
+""",
+    "depends" : ["base",],   
+    "data" : ["natuurpunt_ir_ui_view_page_view.xml","natuurpunt_ir_ui_view_page_cron.xml",],
+    "init_xml" : [],
+    "update_xml" : [],
+    "active": False,
+    "installable": True
+}
